65d464c1f9aa7535c07c46722aeb60d699590436
yas-mnkornym <yasuhiro@aurora.ocn.ne.jp>
2014-10-07 15:16:42 +0900

    Added Monitor

---
+++ DroneManipulator/DroneManipulator.Core/IDronesMonitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneManipulator
+{
+	public interface IDronesMonitor
+	{
+		/// <summary>
+		/// Gets the monitor control as a WPF control.
+		/// </summary>
+		object Control { get; }
+
+		/// <summary>
+		/// Gets the control for manipulation monitor as a WPF control.
+		/// </summary>
+		object ManipulatorControl { get; set; }
+
+		/// <summary>
+		/// Updates drones posture.
+		/// </summary>
+		/// <param name="drones"></param>
+		void Update(DronePosture[] drones);
+	}
+}
